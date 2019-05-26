2794c571f8ae37af0449f04ce0d4c2dfa421391a
Oksana Plotnikova <oksankapl17@gmail.com>
2019-05-26 20:46:56 +0300

    Minor changes

--- src/components/TopBar/styles.js
+++ src/components/TopBar/styles.js
@@ -1,6 +1,7 @@
-export const styles = () => ({
+export const styles = ({spacing: {unit}}) => ({
   root: {
     width: "100%",
+    marginBottom: unit * 2,
   },
   grow: {
     flexGrow: 1,
--- src/constants/locales.js
+++ src/constants/locales.js
@@ -1,11 +1,11 @@
 export const locale = {
   WELCOME: "Welcome to Nodus research",
-  FAVOURITE_ITEMS: "My favourite search items",
   SEARCH: "Search",
-  ADD_TO_FAVOURITES: "Add to favourites",
   SEARCH_IN_PUBMED: "Search in PubMed",
-  NO_MATCHES: "No matches found.",
   TOO_SHORT_SEARCH_QUERY: "Please enter more than 3 characters for search.",
+  NO_MATCHES: "No matches found.",
+  FAVOURITE_ITEMS: "My favourite search items",
+  ADD_TO_FAVOURITES: "Add to favourites",
   NO_FAVOURITES:
-    "You don't have any favourites at the moment. You can add them at anytime.",
+    "You don't have any favourites at the moment. You can add them at any time.",
 };
