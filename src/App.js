699b2cf352ecc03959adc8dad4b844d1e22facfc
maksym,plotnikov <maksym.plotnikov@lemberg.co.uk>
2019-05-27 10:10:15 +0300

    Add redux-persist for favourites

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from "react";
 import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
+import persistStore from "./utils/store/store";
 // Components
 import TopBar from "./components/TopBar";
 import Search from "./components/Search";
 import FavouriteItems from "./components/FavouriteItems";
 import Notification from "./components/Notification";
 import Articles from "./components/Articles";
-// Utils
-import store from "./utils/store/store";
 // Constants
 import {locale} from "./constants/locales";
 import "./App.css";
 
+const {store, persistor} = persistStore();
+
 function App() {
   return (
     <Provider store={store}>
-      <div>
+      <PersistGate loading={null} persistor={persistor}>
         <Notification />
         <TopBar heading={locale.WELCOME} />
         <Search />
         <FavouriteItems heading={locale.FAVOURITE_ITEMS} />
         <Articles />
-      </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/utils/store/store.js
+++ src/utils/store/store.js
@@ -1,15 +1,27 @@
 import {createStore, applyMiddleware, compose} from "redux";
-
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import rootReducer from "../../reducers";
 
-const initialState = {};
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["favourites"],
+};
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeEnhancers(applyMiddleware(thunk)),
-);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default store;
+export default () => {
+  const store = createStore(
+    persistedReducer,
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__(),
+    ),
+  );
+  const persistor = persistStore(store);
+
+  return {store, persistor};
+};
